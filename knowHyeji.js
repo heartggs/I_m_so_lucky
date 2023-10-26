0ebf019076bcba68e7597743e846e57781471eea
heartggs <jounghj0806@naver.com>
2023-10-26 19:40:01 +0900

    1st commit

---
+++ knowHyeji.js
@@ -0,0 +1,70 @@
+const explanationPopup = {
+  hyeji: {
+    title: "정혜지",
+    content: [
+      "Name : 정혜지",
+      "Phone : 010 - 3192 - 6407",
+      "E-Mail : jounghj0806@naver.com",
+      "Blog : https://blog.naver.com/heather_blog",
+      "Github : https://github.com/heartggs",
+    ],
+    img: "./image/person_me.png",
+  },
+  shirt: {
+    title: "SI프로젝트 1년 경력 (조직 경험 보유)",
+    content: [
+      "si프로젝트를 퍼블리셔로 활동한 경력있음",
+      "기획 / 디자인 / 개발자와 같은 타 직무군과의 대화를 통해 프로젝트를 경험",
+      "소통과 협업능력이 다른 신입에 비해 좀 더 뛰어날 것으로 예상",
+    ],
+    img: "./image/shirt_me.png",
+  },
+  pants: {
+    title: "프론트엔드개발자 정혜지",
+    content: [
+      "코딩은 한번에 정적 / 동적인 것을 만들수 있다",
+      "이러한 점이 흥미롭게 다가와 프론트엔드 개발자를 선택",
+      "프론트엔드 개발자가 되기 위해 열심히 노력중",
+    ],
+    img: "./image/jeans_me.png",
+  },
+  jacket: {
+    title: "동적인 것에 큰 호기심",
+    content: [
+      "동적인 인터랙티브 웹 만들기에 큰 호기심을 가지고 있다",
+      "세상에서 가장 멋진 인터랙티브 웹을 만들것이다",
+    ],
+    img: "./image/t-shirt_me.png",
+  },
+  dress: {
+    title: "백엔드와의 팀프로젝트를 경험",
+    content: [
+      "RestfulAPI를 활용하여 서비스 구축",
+      "스프링을 주 언어로 사용한 백엔드와 기획부터, 개발, 배포까지 전과정 경험",
+      "화면크기에 따른 반응형 웹페이지로 개발",
+    ],
+    img: "./image/hoodie_me.png",
+  },
+  shoe: {
+    title: "팀프로젝트(Pop-it!)",
+    content: [
+      "팝업스토어 소재의 이커머스 서비스",
+      "사용스택 : React, Typescript, tailwindCSS, Axios, Json data, Vite",
+      "참여인원 : 프론트엔드 3명 / 백엔드 4명",
+      "https://silent-mile-eee.notion.site/POP-IT-47e72bd78e994767b01ae94fcdaa2cf4?pvs=4",
+    ],
+    img: "./image/shoe_me.png",
+  },
+  cap: {
+    title: "개인프로젝트(caff캪caff)",
+    content: [
+      "카페인에 관심이 많은 사람들을 위해",
+      "음료의 카페인 함량과 개인의 권장 섭취량 비율을 확인할 수 있는 서비스",
+      "사용스택 : React, Typescript, Axios, React-router-dom, vite, Firebase",
+      "https://silent-mile-eee.notion.site/caff-caff-0358eda68b0749f7ace1a340a04bb01f?pvs=4",
+    ],
+    img: "./image/cap_me.png",
+  },
+};
+
+export { explanationPopup };
